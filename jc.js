62f021ecf2c44c457d2668d6369d99ef1e15bdcf
khojakapro <azam188003@gmail.com>
2020-12-17 12:53:30 +0500

    kkk

---
+++ jc.js
@@ -0,0 +1,14 @@
+let x= +prompt('Neshta')
+let y,y1
+for(let i=1;i<=x;i++){
+    
+    if(i==1){
+        y='1 qoy. . . '
+    }else{
+        y1=(i)+' qoylar . . . '
+        y=y+y1
+
+    }
+    
+}
+console.log(y);
